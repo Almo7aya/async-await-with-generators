22d50e4be779045ac70744d6e6f1995988b89ad2
Ali Almohaya <alialmohaya@gmail.com>
2020-07-26 13:10:57 +0200

    Refactor the code

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-// ============= Reslover function ============= //
-
-const resloveAwaiter = (generator) => {
-    // Fire generator for the first time
-    const { value, done } = generator.next();
-    if (done) return; // Check if it's done
-    const reslover = (asyncFn) => {
-        if (asyncFn.then) {
-            asyncFn.then(data => {
-                const {value, done} = generator.next(data);
-                // Check if the generator is not finished yet and call reslover recursively
-                if (!done) reslover(value);
-            });
-        } else if (typeof asyncFn === 'function') { // Must be a cb
-            asyncFn(data => {
-                const {value, done} = generator.next(data);
-                // Check if the generator is not finished yet and call reslover recursively
-                if (!done) reslover(value);
-            });
-        }
-    }
-    reslover(value);
-}
-resloveAwaiter(asyncContext()); // Invoke the reslover right away
-
 // ============= Fake methods ============= //
 
 function output(data) {
@@ -31,19 +6,19 @@ function output(data) {
     document.body.appendChild(p);
 }
 
-function fakeLongPromise (){
+function fakeLongPromise() {
     return new Promise(res => {
         setTimeout(res.bind(null, 'Hi from promise!'), 1000);
     });
 }
 
-function fakeLongCb (cb) {
+function fakeLongCb(cb) {
     setTimeout(() => cb('Hi from cb!'), 1000);
 }
 
 // ============= async ============= //
 
-function* asyncContext() {
+resloveAwaiter(function* asyncContext() {
     let data = yield fakeLongPromise();
     output(data);
 
@@ -69,4 +44,4 @@ function* asyncContext() {
     output(data);
 
     output('DONE !');
-}
+});
